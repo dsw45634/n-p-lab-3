f0eb27c8c52398055291daebcb587c669becf428
Zdzimi <michalzdziennickip@gmail.com>
2022-06-27 13:37:38 +0200

    Add help info

--- skrypt.sh
+++ skrypt.sh
@@ -16,7 +16,18 @@ createLogs ()
 	done
 }
 
+helpInfo ()
+{
+	echo "$0"
+	echo "Opcje:"
+	echo "--date - wyświetl aktualną datę"
+	echo "--logs - utwórz 100 plików log(1 - 100).txt"
+	echo "--logs X - utwórz X plików log(1 - X).txt"
+	echo "--help - wyświetl tą informację."
+}
+
 case "$1" in
 	"--date") date +%F;;
 	"--logs") createLogs "$2";;
+	"--help") helpInfo;;
 esac
