667be08399623ba6ff19ef68cebd9816ff656f94
Zdzimi <michalzdziennickip@gmail.com>
2022-06-27 13:02:34 +0200

    Add create 100 log files

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+createLogs ()
+{
+	for i in `seq 1 100`
+	do
+		echo log"$i".txt > log"$i".txt
+		echo "$0" >> log"$i".txt
+		date +%F >> log"$i".txt
+	done
+}
+
 case "$1" in
 	"--date") date +%F;;
+	"--logs") createLogs;;
 esac
-	
